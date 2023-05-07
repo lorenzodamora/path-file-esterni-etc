7a77242153527dd0757b0f0fc6ce72233b7e64c6
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-05-07 03:54:06 +0200

    file accesso diretto  modifiche minori a F3()

--- 8 file accesso diretto/Program.cs
+++ 8 file accesso diretto/Program.cs
@@ -33,10 +33,10 @@ namespace file_accesso_diretto
 		}
 		static string GetPath()
 		{
-			string path = Path.GetFullPath(".");
-			path = Path.GetDirectoryName(path);
-			path = Path.GetDirectoryName(path);
-			path += @"\vari test\testo";
+			string path = Path.GetFullPath("..\\..\\vari test\\testo");
+			//path = Path.GetDirectoryName(path);
+			//path = Path.GetDirectoryName(path);
+			//path += @"\vari test\testo";
 			// Delete a directory and all subdirectories with Directory static method...
 			if (System.IO.Directory.Exists(path))
 				try
@@ -84,7 +84,7 @@ namespace file_accesso_diretto
 			string path2 = GetPath() + "\\" + Path.GetFileName(pat2);
 			File.Move(pat2, path2);
 			Console.WriteLine(path2);
-			using (FileStream fs2 = File.Open(path2, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+			using (FileStream fs2 = new FileStream(path2, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
 			{
 				Byte[] info = new UTF8Encoding(true).GetBytes("0\n1\n2\n3\n4\n5\n6\n7\n8\n9");
 				for (int i = 0; i < info.Length; i++)
@@ -121,60 +121,58 @@ namespace file_accesso_diretto
 			string path3 = GetPath() + "\\" + Path.GetFileName(pat3);
 			File.Move(pat3, path3);
 			Console.WriteLine(path3);
-			using (FileStream fs3 = File.Open(path3, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+			FileStream fs3 = new FileStream(path3, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+
+			//se il file è vuoto scrivere semplicemente la stringa e convertirla in byte
+			string str = "test\nacaso\n0123456789\n\nciaogente";
+			Byte[] info = new UTF8Encoding(true).GetBytes(str);
+			fs3.Write(info, 0, info.Length);
+
+			//per leggere il file già pieno
+			byte[] b = new byte[info.Length];
+			UTF8Encoding temp = new UTF8Encoding(true);
+
+			fs3.Seek(0, SeekOrigin.Begin); // torna all'inizio col cursore
+			fs3.Read(b, 0, b.Length); //sembra legga tutto in un iterazione
+			/*fs3.Read(b, 2, b.Length);*/ //va oltre i limiti della matrice
+			/*fs3.Read(b, 2, b.Length-2);*/ //stampa 2 spazi, poi tutto il file,ma salta le ultime due lettere
+			Console.WriteLine(temp.GetString(b));
+			Console.WriteLine("\\\\\n");
+
+			fs3.Seek(0, SeekOrigin.Begin);
+			//legge e stampa byte per byte una riga, la prima,, il writeline finale sostituisce \n non stampato
+			for (int ch = fs3.ReadByte(); ch!=-1; ch = fs3.ReadByte()) //   fs3.Position < fs3.Length   =è uguale a=   ch!=-1
 			{
-				string str = "test\nacaso\n0123456789\n\nciaogente";
-				Byte[] info = new UTF8Encoding(true).GetBytes(str);
-				fs3.Write(info, 0, info.Length);
-
-				byte[] b = new byte[info.Length];
-				UTF8Encoding temp = new UTF8Encoding(true);
-
-				fs3.Seek(0, SeekOrigin.Begin);
-				while (fs3.Read(b, 0, b.Length)>0) //in read offset è in base a tutto il file? o ai byte?
-					Console.WriteLine(temp.GetString(b));
-				Console.WriteLine("\\\\\n");
-
-				fs3.Seek(0, SeekOrigin.Begin);
-				//legge e stampa una riga, il writeline finale sostituisce \n non stampato
-				for (int ch = fs3.ReadByte(); ch!=-1; ch = fs3.ReadByte()) //fs3.Position < fs3.Length  ==   ch!=-1
-				{
-					Console.Write((char)ch);
-					if ((char)ch == '\n') break;
-				}
-
-				fs3.Seek(0, SeekOrigin.Begin);
-				//legge solo la riga scelta (3a)
-				int line = 0;
-				for (int ch = fs3.ReadByte(); ch!=-1 && line != 3; ch = fs3.ReadByte())
-				{
-					if (line == 2) Console.Write((char)ch);
-					if ((char)ch == '\n') line++;
-				} //va a capo una volta finita la riga
-				  //Console.WriteLine("");
-
-				//leggi una parte della riga scelta (3a) "0123456789\n", (3456)
-				line=0;
-				int chc = 0; //char count
-				fs3.Seek(0, SeekOrigin.Begin);
-				for (int ch = fs3.ReadByte(); ch!=-1 && line != 3; ch = fs3.ReadByte())
-				{
-					if (line == 2)
-					{
-						if (chc >= 3 && chc <=6) Console.Write((char)ch);
-						chc++;
-					}
-					if ((char)ch == '\n') line++;
-				}
-				//oppure porto tutta la riga a stringa e leggo la parte che voglio (utile per lavorarci)
-				Console.WriteLine();
-
+				Console.Write((char)ch);
+				if ((char)ch == '\n') break;
 			}
-		}
 
+			fs3.Seek(0, SeekOrigin.Begin);
+			//legge solo la riga scelta (3a) //la terza linea è la indice 2
+			for (int ch = fs3.ReadByte(), line = 0; ch!=-1 && line != 3; ch = fs3.ReadByte()) //
+			{
+				if (line == 2) Console.Write((char)ch);
+				if ((char)ch == '\n') line++;
+			} //va a capo una volta finita la riga
+			  //Console.WriteLine("");
+
+			//leggi una parte della riga scelta (3a) "0123456789\n", (3456) //avendo l'indice caratteri
+			//int chc = 0; //char count
+			fs3.Seek(0, SeekOrigin.Begin);
+			for (int ch = fs3.ReadByte(), line = 0, chc = 0; ch!=-1 && line != 3; ch = fs3.ReadByte())
+			{
+				if (line == 2)
+				{
+					if (chc >= 3 && chc <=6) Console.Write((char)ch);
+					chc++;
+				}
+				if ((char)ch == '\n') line++;
+			}
+			//oppure porto tutta la riga a stringa e leggo la parte che voglio (utile per lavorarci)
+			Console.WriteLine();
+
+			//importante
+			fs3.Close();
+		}
 	}
 }
-
-
-
-
